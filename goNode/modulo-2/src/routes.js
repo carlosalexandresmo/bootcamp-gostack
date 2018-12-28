5c55ca2ae430e02176ef41e6ffec8988971c9038
Sérgio Deusdedith de Araujo Neto <sergio.deusdedith@gmail.com>
2018-12-27 21:48:21 -0300

    Tentando autenticação

--- goNode/modulo-2/src/routes.js
+++ goNode/modulo-2/src/routes.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const routes = express.Router()
 const UserController = require('./app/controllers/UserController')
+const SessionController = require('./app/controllers/UserController')
 const multerConfig = require('./config/multer')
 const upload = require('multer')(multerConfig)
+
+routes.get('/', SessionController.create)
+routes.post('/', SessionController.store)
 
 routes.get('/signup', UserController.create)
 routes.post('/signup', upload.single('avatar'), UserController.store)
